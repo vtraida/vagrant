1e2e17f8e2b84215473088a63d2df007a3a6d45a
Vadym <vtraida@gmail.com>
2018-03-06 01:33:20 +0200

    testing sh

---
+++ provision.sh
@@ -0,0 +1,1 @@
+#!/usr/bin/env bashecho "Installing Apache and setting it up..."apt-get update >/dev/null 2>&1apt-get install -y apache2 >/dev/null 2>&1rm -rf /var/wwwln -fs /host_files /var/www/html
